d522f62a24722a316ed067b363ceb222ef415508
Brian Rapp <Brian.Rapp@noaa.gov>
2018-03-23 12:17:40 -0400

    Fixed mi6 host telemetry misconfigurations

--- shelby/cfg/mi6_config.py
+++ shelby/cfg/mi6_config.py
@@ -54,7 +54,7 @@ job['hattr'] = {}
 job['hattr'][1] = {}
 job['hattr'][1]['collect']     = []
 job['hattr'][1]['collect']     = ['filesystems', 'cpu', 'memory', 'network']
-job['hattr'][1]['network']     = [('netpref', 'eno')]
+job['hattr'][1]['network']     = [('netname', 'eth0')]
 job['hattr'][1]['filesystems'] = []
 job['hattr'][1]['filesystems'].append({'fs': '/'})
 job['hattr'][1]['filesystems'].append({'fs': '/dev/shm', 'collect': True})
@@ -68,9 +68,9 @@ job['hattr'][1]['filesystems'].append({'fs': '/var/log'})
 job['hattr'][1]['filesystems'].append({'fs': '/var/log/audit'})
 
 job['hattr'][2] = {}
-job['hattr'][1]['collect']     = []
+job['hattr'][2]['collect']     = []
 job['hattr'][2]['collect']     = ['filesystems', 'cpu', 'memory', 'network']
-job['hattr'][2]['network']     = [('netpref', 'eno')]
+job['hattr'][2]['network']     = [('netname', 'eth0')]
 job['hattr'][2]['filesystems'] = []
 job['hattr'][2]['filesystems'].append({'fs': '/'})
 job['hattr'][2]['filesystems'].append({'fs': '/dev/shm', 'collect': True})
@@ -82,125 +82,3 @@ job['hattr'][2]['filesystems'].append({'fs': '/usr1'})
 job['hattr'][2]['filesystems'].append({'fs': '/var'})
 job['hattr'][2]['filesystems'].append({'fs': '/var/log'})
 job['hattr'][2]['filesystems'].append({'fs': '/var/log/audit'})
-
-job['hattr'][3] = {}
-job['hattr'][3]['collect']     = ['filesystems', 'cpu', 'memory', 'network']
-job['hattr'][3]['network']     = [('netpref', 'eno')]
-job['hattr'][3]['filesystems'] = []
-job['hattr'][3]['filesystems'].append({'fs': '/'})
-job['hattr'][3]['filesystems'].append({'fs': '/dev/shm', 'collect': True})
-job['hattr'][3]['filesystems'].append({'fs': '/opt'})
-job['hattr'][3]['filesystems'].append({'fs': '/scratch'})
-job['hattr'][3]['filesystems'].append({'fs': '/tmp'})
-job['hattr'][3]['filesystems'].append({'fs': '/usr'})
-job['hattr'][3]['filesystems'].append({'fs': '/usr1'})
-job['hattr'][3]['filesystems'].append({'fs': '/var'})
-job['hattr'][3]['filesystems'].append({'fs': '/var/log'})
-job['hattr'][3]['filesystems'].append({'fs': '/var/log/audit'})
-
-job['hattr'][4] = {}
-job['hattr'][4]['collect']     = ['filesystems', 'cpu', 'memory', 'network']
-job['hattr'][4]['network']     = [('netpref', 'eno')]
-job['hattr'][4]['filesystems'] = []
-job['hattr'][4]['filesystems'].append({'fs': '/'})
-job['hattr'][4]['filesystems'].append({'fs': '/dev/shm', 'collect': True})
-job['hattr'][4]['filesystems'].append({'fs': '/opt'})
-job['hattr'][4]['filesystems'].append({'fs': '/scratch'})
-job['hattr'][4]['filesystems'].append({'fs': '/tmp'})
-job['hattr'][4]['filesystems'].append({'fs': '/usr'})
-job['hattr'][4]['filesystems'].append({'fs': '/usr1'})
-job['hattr'][4]['filesystems'].append({'fs': '/var'})
-job['hattr'][4]['filesystems'].append({'fs': '/var/log'})
-job['hattr'][4]['filesystems'].append({'fs': '/var/log/audit'})
-
-job['hattr'][5] = {}
-job['hattr'][5]['collect']     = ['filesystems', 'cpu', 'memory', 'network']
-job['hattr'][5]['network']     = [('netpref', 'eno')]
-job['hattr'][5]['filesystems'] = []
-job['hattr'][5]['filesystems'].append({'fs': '/'})
-job['hattr'][5]['filesystems'].append({'fs': '/dev/shm', 'collect': True})
-job['hattr'][5]['filesystems'].append({'fs': '/opt'})
-job['hattr'][5]['filesystems'].append({'fs': '/scratch'})
-job['hattr'][5]['filesystems'].append({'fs': '/tmp'})
-job['hattr'][5]['filesystems'].append({'fs': '/usr'})
-job['hattr'][5]['filesystems'].append({'fs': '/usr1'})
-job['hattr'][5]['filesystems'].append({'fs': '/var'})
-job['hattr'][5]['filesystems'].append({'fs': '/var/log'})
-job['hattr'][5]['filesystems'].append({'fs': '/var/log/audit'})
-
-job['hattr'][6] = {}
-job['hattr'][6]['collect']     = ['filesystems', 'cpu', 'memory', 'network']
-job['hattr'][6]['network']     = [('netpref', 'eno')]
-job['hattr'][6]['filesystems'] = []
-job['hattr'][6]['filesystems'].append({'fs': '/'})
-job['hattr'][6]['filesystems'].append({'fs': '/dev/shm', 'collect': True})
-job['hattr'][6]['filesystems'].append({'fs': '/opt'})
-job['hattr'][6]['filesystems'].append({'fs': '/scratch'})
-job['hattr'][6]['filesystems'].append({'fs': '/tmp'})
-job['hattr'][6]['filesystems'].append({'fs': '/usr'})
-job['hattr'][6]['filesystems'].append({'fs': '/usr1'})
-job['hattr'][6]['filesystems'].append({'fs': '/var'})
-job['hattr'][6]['filesystems'].append({'fs': '/var/log'})
-job['hattr'][6]['filesystems'].append({'fs': '/var/log/audit'})
-
-job['hattr'][7] = {}
-job['hattr'][7]['collect']     = ['filesystems', 'cpu', 'memory', 'network']
-job['hattr'][7]['network']     = [('netpref', 'eno')]
-job['hattr'][7]['filesystems'] = []
-job['hattr'][7]['filesystems'].append({'fs': '/'})
-job['hattr'][7]['filesystems'].append({'fs': '/dev/shm', 'collect': True})
-job['hattr'][7]['filesystems'].append({'fs': '/opt'})
-job['hattr'][7]['filesystems'].append({'fs': '/scratch'})
-job['hattr'][7]['filesystems'].append({'fs': '/tmp'})
-job['hattr'][7]['filesystems'].append({'fs': '/usr'})
-job['hattr'][7]['filesystems'].append({'fs': '/usr1'})
-job['hattr'][7]['filesystems'].append({'fs': '/var'})
-job['hattr'][7]['filesystems'].append({'fs': '/var/log'})
-job['hattr'][7]['filesystems'].append({'fs': '/var/log/audit'})
-
-job['hattr'][8] = {}
-job['hattr'][8]['collect']     = ['filesystems', 'cpu', 'memory', 'network']
-job['hattr'][8]['network']     = [('netpref', 'eno')]
-job['hattr'][8]['filesystems'] = []
-job['hattr'][8]['filesystems'].append({'fs': '/'})
-job['hattr'][8]['filesystems'].append({'fs': '/dev/shm', 'collect': True})
-job['hattr'][8]['filesystems'].append({'fs': '/opt'})
-job['hattr'][8]['filesystems'].append({'fs': '/scratch'})
-job['hattr'][8]['filesystems'].append({'fs': '/tmp'})
-job['hattr'][8]['filesystems'].append({'fs': '/usr'})
-job['hattr'][8]['filesystems'].append({'fs': '/usr1'})
-job['hattr'][8]['filesystems'].append({'fs': '/var'})
-job['hattr'][8]['filesystems'].append({'fs': '/var/log'})
-job['hattr'][8]['filesystems'].append({'fs': '/var/log/audit'})
-
-job['hattr'][9] = {}
-job['hattr'][9]['collect']     = ['filesystems', 'cpu', 'memory', 'network']
-job['hattr'][9]['network']     = [('netpref', 'eno')]
-job['hattr'][9]['filesystems'] = []
-job['hattr'][9]['filesystems'].append({'fs': '/'})
-job['hattr'][9]['filesystems'].append({'fs': '/dev/shm', 'collect': True})
-job['hattr'][9]['filesystems'].append({'fs': '/opt'})
-job['hattr'][9]['filesystems'].append({'fs': '/scratch'})
-job['hattr'][9]['filesystems'].append({'fs': '/tmp'})
-job['hattr'][9]['filesystems'].append({'fs': '/usr'})
-job['hattr'][9]['filesystems'].append({'fs': '/usr1'})
-job['hattr'][9]['filesystems'].append({'fs': '/var'})
-job['hattr'][9]['filesystems'].append({'fs': '/var/log'})
-job['hattr'][9]['filesystems'].append({'fs': '/var/log/audit'})
-
-job['hattr'][10] = {}
-job['hattr'][10]['collect']     = ['filesystems', 'cpu', 'memory', 'network']
-job['hattr'][10]['network']     = [('netpref', 'eno')]
-job['hattr'][10]['filesystems'] = []
-job['hattr'][10]['filesystems'].append({'fs': '/'})
-job['hattr'][10]['filesystems'].append({'fs': '/data'})
-job['hattr'][10]['filesystems'].append({'fs': '/dev/shm', 'collect': True})
-job['hattr'][10]['filesystems'].append({'fs': '/miniconda'})
-job['hattr'][10]['filesystems'].append({'fs': '/opt'})
-job['hattr'][10]['filesystems'].append({'fs': '/scratch'})
-job['hattr'][10]['filesystems'].append({'fs': '/tmp'})
-job['hattr'][10]['filesystems'].append({'fs': '/usr'})
-job['hattr'][10]['filesystems'].append({'fs': '/usr1'})
-job['hattr'][10]['filesystems'].append({'fs': '/var'})
-job['hattr'][10]['filesystems'].append({'fs': '/var/log'})
-job['hattr'][10]['filesystems'].append({'fs': '/var/log/audit'})
