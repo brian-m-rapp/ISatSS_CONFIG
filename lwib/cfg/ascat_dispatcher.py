a815cb3b44a9246b03e0312de8eabaf5389ec116
Brian Rapp <Brian.Rapp@noaa.gov>
2019-03-11 17:02:06 +0000

    Use remote_puller with 'CP' protocol to be able to use ledger

--- lwib/cfg/ascat_dispatcher.py
+++ lwib/cfg/ascat_dispatcher.py
@@ -55,29 +55,23 @@ job['data']['log']['method']          = {'technique':'inplace'}
 job['data']['log']['schedule']        = {'interval':3600}
 job['data']['log']['activeonly']      = True
 
-
 job['notifications']   = {}
 job['notifications']['nhc']   = {'node':273, 'enabled':True, 'fields':['file', 'node'], 'prefix':'ascat_'}
 
-
 job['sources'] = {}
-job['sources']['ascat'] = {'protocol':'CP', 'paths':{}}
+job['sources']['ascat'] =  {'protocol':'CP', 'timeout':10, 'retry':10,'paths':{},'sessions':1}
 
 ascata_args = {'window':3600, 'cyclecount':None}
 ascata_args['target'] = {'data':job['data']['afiles'], 'notifications':job['notifications']}
 ascata_args['ledger'] = {'node':job['data']['aledger']['location']['node'],'name':'ascata.ledger'}
+job['sources']['ascat']['paths']['ascata'] = {'path':'/home/lbyerle/checkisatss/pullFromPDA/Global/ASCAT/metopa', 'dirs':{}, 'special':{}}
+job['sources']['ascat']['paths']['ascata']['special'] = {'module':'remote_puller','class':'FilePuller','args':ascata_args}
 
 ascatb_args = {'window':3600, 'cyclecount':None}
 ascatb_args['target'] = {'data':job['data']['afiles'], 'notifications':job['notifications']}
 ascatb_args['ledger'] = {'node':job['data']['aledger']['location']['node'],'name':'ascatb.ledger'}
-
-job['sources']['ascat']['paths']['dropzone'] = {'node':270, 'files':{}, 'delete':False}
-job['sources']['ascat']['paths']['dropzone']['files']['ascata']  = {'retrieve':{}, 'args':ascata_args}
-job['sources']['ascat']['paths']['dropzone']['files']['ascata']['retrieve']['nhc']  = {'enabled':True, 'dataitem':'afiles',  'notifications':['nhc']}
-
-job['sources']['ascat']['paths']['dropzone'] = {'node':271, 'files':{}, 'delete':False}
-job['sources']['ascat']['paths']['dropzone']['files']['ascatb']  = {'retrieve':{}, 'args':ascatb_args}
-job['sources']['ascat']['paths']['dropzone']['files']['ascatb']['retrieve']['nhc']  = {'enabled':True, 'dataitem':'afiles',  'notifications':['nhc']}
+job['sources']['ascat']['paths']['ascatb'] = {'path':'/home/lbyerle/checkisatss/pullFromPDA/Global/ASCAT/metopb', 'dirs':{}, 'special':{}}
+job['sources']['ascat']['paths']['ascatb']['special'] = {'module':'remote_puller','class':'FilePuller','args':ascatb_args}
 
 
 job['monitor'] = {'agents':{},'mi6':{}}
