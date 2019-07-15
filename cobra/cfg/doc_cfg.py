00d39aac87da2b8a189345e7b044f52626845229
Brian Rapp <Brian.Rapp@noaa.gov>
2019-07-15 18:17:11 +0000

    doc updates

--- cobra/cfg/doc_cfg.py
+++ cobra/cfg/doc_cfg.py
@@ -7,6 +7,17 @@ articles['shaper_install']              = {}
 articles['shaper_install']['document']  = 'shaper_install.html'
 articles['shaper_install']['articleid'] = 0
 articles['shaper_install']['version']   = '1.0'
+"""
+articles                              = {}
+articles['install']                    = {}
+articles['install']['document']        = 'dev_install.html'
+articles['install']['articleid']       = 4200844
+articles['install']['version']         = '1.0'
+articles['configs']                    = {}
+articles['configs']['document']        = 'dev_configurations.html'
+articles['configs']['articleid']       = 3745830
+articles['configs']['version']         = '1.0'
+"""
 
 defaults            = {}
 defaults['credid']  = 0
