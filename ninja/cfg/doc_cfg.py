9ea9749e4e474b25ccf1781e10c4d0c95aaba23f
Brian Rapp <brian.rapp@noaa.gov>
2020-05-14 12:27:35 -0400

    New mailer added

--- ninja/cfg/doc_cfg.py
+++ ninja/cfg/doc_cfg.py
@@ -5,10 +5,10 @@ e.g. -
 vugraf local vedit --article=mailer_2020-05-11 --cfg=/home/brapp/git/ISatSS_CONFIG/ninja/cfg/doc_cfg.py
 
 '''
-articles                                   = {}
-articles['mailer']                 = {}
-articles['mailer']['document']     = 'mailer_body_2020-05-08.html'
-articles['mailer_2020-05-11']                 = {}
-articles['mailer_2020-05-11']['document']     = 'mailer_body_2020-05-11.html'
-articles['mailer_2020-05-14']                 = {}
-articles['mailer_2020-05-14']['document']     = 'mailer_body_2020-05-14.html'
+articles = {}
+
+articles['mailer_2020-05-11'] = {}
+articles['mailer_2020-05-11']['document'] = 'mailer_body_2020-05-11.html'
+
+articles['mailer_2020-05-14'] = {}
+articles['mailer_2020-05-14']['document'] = 'mailer_body_2020-05-14.html'
